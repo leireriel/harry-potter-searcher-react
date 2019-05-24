0a8896ff77ce892033f4fbee185952eec636a63f
leireriel <leirericop@gmail.com>
2019-05-24 14:05:59 +0200

    Create CardList and CardNavigation components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,59 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-// import CardNavigation from './CardNavigation';
+import CardList from './CardList';
+import CardNavigation from './CardNavigation';
 
 class CharacterCard extends Component {
   render() {
     const { param, characters } = this.props;
-    const characterSelected =
-      characters.find(item => item.id === parseInt(param));
-
     return (
       <Fragment>
         <Link to="/">Volver a la lista de personajes</Link>
 
-        {characterSelected ?
-          <ol className="list-card">
-            <li className="name-card">
-              {characterSelected.name}
-            </li>
-            <li className="house-card">
-              Casa: {characterSelected.house}
-            </li>
-            <li className="birth-card">
-              Nacimiento: {characterSelected.yearOfBirth}
-            </li>
-            {`${characterSelected.hogwartsStaff}` ?
-              <li className="category-card">
-                Categoría: {'Empleado en Hogwarts'}
-              </li>
-              :
-              null
-            }
-            {`${characterSelected.hogwartsStudent}` ?
-              <li className="category-card">
-                Categoría: {'Estudiante'}
-              </li>
-              :
-              null
-            }
-            <li className="patronus-card">
-              Patronus: {characterSelected.patronus}
-            </li>
-            <li className="alive-card">
-              Estado:{`${characterSelected.alive}` ? 'VIVO'
-                :
-                'RIP'}
-            </li>
-          </ol>
-          :
-          <p>Cargando...</p>
-        }
-
-        {/* <CardNavigation
+        <CardList
           param={param}
           characters={characters}
-        /> */}
+        />
+
+        <CardNavigation
+          param={param}
+          characters={characters}
+        />
       </Fragment >
     );
   }

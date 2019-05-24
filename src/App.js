6f04d24c9ca48b2d39e245c91c8d2f05edb436f3
leireriel <leirericop@gmail.com>
2019-05-24 09:48:09 +0200

    Add form to find characters

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      characters: [] 
+      characters: []
     }
 
     this.getCharacters = this.getCharacters.bind(this);
@@ -20,21 +20,50 @@ class App extends React.Component {
     fetchCharacters()
       .then(data => {
         const finalData = data.map((item, index) => {
-          return {...item, id: (index)};
+          return { ...item, id: (index) };
         });
 
-        this.setState ({
+        this.setState({
           characters: finalData
-        })
+        });
+        console.log(finalData)
       });
   }
 
   render() {
+    const { characters } = this.state;
     return (
       <React.Fragment>
-        <h1 className="title-hogwarts">Welcome to Hogwarts</h1>
+        <h1 className="title-hogwarts">
+          Welcome to Hogwarts
+        </h1>
+        <form action="">
+          <label htmlFor="characters-finder">
+            Look for your favourite character
+          </label>
+          <input
+            type="text"
+            id="characters-finder"
+          />
+        </form>
         <ol className="list-characters">
-
+          {characters.map((item, index) => {
+            return (
+              <li className="card-character" key={index}>
+                <img
+                  src={item.image}
+                  alt={item.name}
+                />
+                <h2 className="name-character">
+                  {item.name}
+                </h2>
+                <h3 className="house-character">
+                  {item.house}
+                </h3>
+              </li>
+            )
+          })
+          }
         </ol>
       </React.Fragment>
     );

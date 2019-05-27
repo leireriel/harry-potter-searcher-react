6814c5050d9302e3c46f2f8295b8e19cfe49ea72
leireriel <leirericop@gmail.com>
2019-05-27 11:51:46 +0200

    Public in GitHub pages

---
+++ docs/precache-manifest.ccdc75253635813aadb1fd814f9cae82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caee78e6d2bdc4557ebcd515171b60f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d15125e4bd8ff02661",
+    "url": "/static/css/main.144a5ded.chunk.css"
+  },
+  {
+    "revision": "ceeb73c7d818c6b1a812",
+    "url": "/static/js/2.cbb427d8.chunk.js"
+  },
+  {
+    "revision": "b8d15125e4bd8ff02661",
+    "url": "/static/js/main.9ca62130.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50c0ebe1c7b316164b9eb41e4e6a573a",
+    "url": "/static/media/hedwig.50c0ebe1.png"
+  },
+  {
+    "revision": "a0566dac347529a80ff38a51a56e8d56",
+    "url": "/static/media/logo.a0566dac.png"
+  },
+  {
+    "revision": "c13cd2c9c6058d2369d3bfb238b37de3",
+    "url": "/static/media/sky.c13cd2c9.jpg"
+  }
+]);
